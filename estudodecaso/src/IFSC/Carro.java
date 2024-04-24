68210a0f0e7a9cd9662f07403cd31dc13a72434d
Isabelle <isabelle.o2006@aluno.ifsc.edu.br>
2024-04-24 17:05:50 -0300

    Estrutura

---
+++ estudodecaso/src/IFSC/Carro.java
@@ -0,0 +1,54 @@
+package IFSC;
+
+public class Carro {
+
+	public int getNumerodavaga() {
+		return numerodavaga;
+	}
+	public void setNumerodavaga(int numerodavaga) {
+		this.numerodavaga = numerodavaga;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getAnodefabricação() {
+		return anodefabricação;
+	}
+	public void setAnodefabricação(int anodefabricação) {
+		this.anodefabricação = anodefabricação;
+	}
+	public float getPreçodocarro() {
+		return preçodocarro;
+	}
+	public void setPreçodocarro(float preçodocarro) {
+		this.preçodocarro = preçodocarro;
+	}
+	public int getNumerodeportas() {
+		return numerodeportas;
+	}
+	public void setNumerodeportas(int numerodeportas) {
+		this.numerodeportas = numerodeportas;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	private int numerodavaga;
+	private String marca;
+	private String modelo;
+	private int anodefabricação;
+	private float preçodocarro;
+	private int numerodeportas;
+	private String cor;
+}
---
+++ estudodecaso/src/IFSC/Carros.java
@@ -0,0 +1,94 @@
+package IFSC;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Carros {
+
+	public static void main(String[] args) {
+
+		ArrayList<Carros> gerenciamento = new ArrayList<>();
+		Scanner leitura = new Scanner(System.in);
+		
+		int op = 0;
+		while (op != 5) {
+
+			switch (op) {
+			case 1:
+
+				System.out.println("Digite o numero da vaga:");
+				String numerodavaga = leitura.nextLine();
+
+				Integer numVagaInt = Integer.parseInt(numerodavaga);
+
+				Carro car = new Carro();
+
+				car.setNumerodavaga(numVagaInt);
+
+				/////////////////////
+
+				System.out.println("Digite a marca:");
+				String marca = leitura.nextLine();
+
+				car.setMarca(marca);
+
+				///////////////////////
+
+				System.out.println("Digite o modelo:");
+				String modelo = leitura.nextLine();
+
+				car.setModelo(modelo);
+
+				/////////////////////
+
+				System.out.println("Digite o ano de fabricação:");
+				String anodefabricação = leitura.nextLine();
+
+				Integer anoFabricação = Integer.parseInt(anodefabricação);
+
+				car.setAnodefabricação(anoFabricação);
+
+				/////////////////////
+
+				System.out.println("Digite o preço:");
+				String preçodocarro = leitura.nextLine();
+
+				Integer preço = Integer.parseInt(preçodocarro);
+
+				car.setPreçodocarro(preço);
+
+				/////////////////////
+
+				System.out.println("Digite o numero de portas:");
+				String numerodeportas = leitura.nextLine();
+
+				Integer numPortas = Integer.parseInt(numerodeportas);
+
+				car.setNumerodeportas(numPortas);
+
+				/////////////////////
+
+				System.out.println("Digite a cor do carro:");
+				String cor = leitura.nextLine();
+
+				car.setCor(cor);
+
+				/////////////////////
+
+				// adicionar
+				gerenciamento.add(null);
+				break;
+
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+				
+
+			}
+		}
+
+	}
+}
